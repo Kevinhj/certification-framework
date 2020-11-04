9cb40469789f4398d2fecab7ed8ff610c804769e
Kevinhj <khkevinhernandez@gmail.com>
2020-11-03 21:25:03 -0600

    test case 1 - register new customers

--- src/test/java/pageobjects/RegisterPage.java
+++ src/test/java/pageobjects/RegisterPage.java
@@ -1,5 +1,6 @@
 package pageobjects;
 
+import okhttp3.internal.http.RetryAndFollowUpInterceptor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,6 +9,14 @@ public class RegisterPage extends BasePage {
 
     private By firstNameTextBox = By.id("input-firstname");
     private By lastNameTextBox = By.id("input-lastname");
+    private By emailTextBox = By.id("input-email");
+    private By phoneTextBox = By.id("input-telephone");
+    private By passwordTextBox = By.id("input-password");
+    private By passwordConfirmationTextBox = By.id("input-confirm");
+    private By privacyPolicyCheckBox = By.xpath("//input[@type='checkbox']");
+    private By submitButton = By.xpath("//input[@value='Continue']");
+    private By accountCreatedContainer = By.xpath("//*[@id='content']/h1");
+
 
     public RegisterPage(WebDriver driver, String url){
         super(driver, url + "index.php?route=account/register");
@@ -21,8 +30,42 @@ public class RegisterPage extends BasePage {
         return driver.findElement(this.lastNameTextBox);
     }
 
-    public void createCustomer(String firstName, String lastName){
+    public WebElement getEmailTextBox(){
+        return driver.findElement(this.emailTextBox);
+    }
+
+    public WebElement getPhoneTextBox(){
+        return driver.findElement(this.phoneTextBox);
+    }
+
+    public WebElement getPasswordTextBox(){
+        return driver.findElement(this.passwordTextBox);
+    }
+
+    public WebElement getPasswordConfirmationTextBox(){
+        return driver.findElement(this.passwordConfirmationTextBox);
+    }
+
+    public WebElement getPrivatePolicyCheckBox(){
+        return driver.findElement(this.privacyPolicyCheckBox);
+    }
+
+    public WebElement getSubmitButton(){
+        return driver.findElement(this.submitButton);
+    }
+
+    public String getAccountCreatedMessage(){
+        return driver.findElement(this.accountCreatedContainer).getText();
+    }
+
+    public void createCustomer(String firstName, String lastName, String email, String phone, String password, String passwordConfirmation){
         this.getFirstNameTextBox().sendKeys(firstName);
         this.getLastNameTextBox().sendKeys(lastName);
+        this.getEmailTextBox().sendKeys(email);
+        this.getPhoneTextBox().sendKeys(phone);
+        this.getPasswordTextBox().sendKeys(password);
+        this.getPasswordConfirmationTextBox().sendKeys(passwordConfirmation);
+        this.getPrivatePolicyCheckBox().click();
+        this.getSubmitButton().click();
     }
 }
--- src/test/java/tests/RegisterTest.java
+++ src/test/java/tests/RegisterTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageobjects.RegisterPage;
 
@@ -11,9 +12,13 @@ public class RegisterTest extends BaseTest {
     }
 
     @Test(groups = {"regression"})
-    public void testCreateCustomer(){
+    public void testCreateCustomer() throws InterruptedException {
         RegisterPage register = new RegisterPage(driver, getBaseUrl());
         register.goToPage();
-        register.createCustomer("Kevin","Hernandez");
+        register.createCustomer("Kev","Test", "kevinandres.hernandez.jimenez@ucreativa.com",
+                "8005541965","test12345", "test12345");
+
+        Thread.sleep(5000);
+        Assert.assertEquals(register.getAccountCreatedMessage(), "Your Account Has Been Created!");
     }
 }
